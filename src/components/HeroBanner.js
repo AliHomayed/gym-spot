e79356927e1c71c1a833546cda934695fbbadc0b
AliHomayed <ali98homayed@gmail.com>
2023-06-06 14:05:07 +0300

    edits

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -11,10 +11,11 @@ const HeroBanner = () => (
       And Repeat
     </Typography>
     <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
-      Check out the most effective exercises personalized to you
+      Check out the most effective exercises personalized for you.
     </Typography>
-    <Stack>
+    <Stack direction="row" gap="120px">
       <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
+      <a href="https://www.calculator.net/bmi-calculator.html" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>BMI Calculator</a>
     </Stack>
     <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
       Exercise
